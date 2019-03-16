f2449c15d7a225220f08da155eb69d85d312c046
kvtoraman <root@CS330-84.(none)>
2019-03-16 16:12:21 +0900

    ignore and remove build files (.o, .d, .output, etc)

--- src/threads/build/lib/kernel/debug.d
+++
@@ -1,5 +0,0 @@
-lib/kernel/debug.o: ../../lib/kernel/debug.c ../../lib/debug.h \
- ../../lib/kernel/console.h ../../lib/stdarg.h ../../lib/stdbool.h \
- ../../lib/stddef.h ../../lib/stdio.h ../../lib/stdint.h \
- ../../lib/kernel/stdio.h ../../lib/string.h ../../threads/init.h \
- ../../threads/interrupt.h ../../devices/serial.h
