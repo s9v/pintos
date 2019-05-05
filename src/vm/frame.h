1db60e8b747ff1f09828f9620beb9d78d818cb3a
Silap Aliyev <sylapaliyev@gmail.com>
2019-05-05 20:09:55 +0900

    Add provided skeleton code

---
+++ src/vm/frame.h
@@ -0,0 +1,14 @@
+#ifndef VM_FRAME_H
+#define VM_FRAME_H
+
+struct frame_table_entry
+{
+	uint32_t* frame;
+	struct thread* owner;
+	struct sup_page_table_entry* spte;
+};
+
+void frame_init (void);
+bool allocate_frame (void *addr);
+
+#endif /* vm/frame.h */
