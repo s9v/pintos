d5492919e5e51cda9eacd81bcd228be83a8fb5a7
Silap Aliyev <sylapaliyev@gmail.com>
2019-06-02 23:50:28 +0900

    34 tests failing. 62.7/110 pts

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -1,6 +1,7 @@
 #include "devices/disk.h"
 #include "devices/timer.h"
 #include "threads/malloc.h"
+#include "threads/synch.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -15,6 +16,8 @@ struct cache_item {
   uint8_t data[DISK_SECTOR_SIZE];
 };
 
+struct lock cache_lock;
+
 // struct cache_item cache[CACHE_ITEMS]; // TODO: malloc instead, if binary size is too big
 struct cache_item *cache; // TODO: malloc instead, if binary size is too big
 
@@ -27,6 +30,7 @@ void cache_discard (int idx);
 
 void cache_init (void) {
   cache = (struct cache_item *) calloc (CACHE_ITEMS, sizeof (struct cache_item));
+  lock_init (&cache_lock);
 
   int i;
   for (i = 0; i < CACHE_ITEMS; i++)
@@ -34,6 +38,7 @@ void cache_init (void) {
 }
 
 void cache_read (struct disk *disk, disk_sector_t sector, void *buffer) {
+  lock_acquire (&cache_lock);
   // printf ("CACHE READ  <<<===   disk: %p   sector: %d\n", disk, sector);
   int idx = cache_lookup (disk, sector);
 
@@ -42,9 +47,11 @@ void cache_read (struct disk *disk, disk_sector_t sector, void *buffer) {
 
   memcpy (buffer, cache[idx].data, DISK_SECTOR_SIZE);
   cache[idx].access_time = timer_ticks ();
+  lock_release (&cache_lock);
 }
 
 void cache_write (struct disk *disk, disk_sector_t sector, const void *buffer) {
+  lock_acquire (&cache_lock);
   // printf ("CACHE WRITE ===>>>   disk: %p   sector: %d\n", disk, sector);
   int idx = cache_lookup (disk, sector);
 
@@ -54,6 +61,7 @@ void cache_write (struct disk *disk, disk_sector_t sector, const void *buffer) {
   memcpy (cache[idx].data, buffer, DISK_SECTOR_SIZE);
   cache[idx].dirty = true;
   cache[idx].access_time = timer_ticks ();
+  lock_release (&cache_lock);
 }
 
 void cache_flush (void) {
